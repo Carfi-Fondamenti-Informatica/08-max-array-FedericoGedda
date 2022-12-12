772d5c31370d61252b6d10b584d2e12926265f04
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-12-12 09:06:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+float mass(int n,float a[]){
+    float m;
+    for (int c=0;c<n;c++){
+        if(c==0){
+            m=a[c];
+        } else if (a[c]>m) {
+            m=a[c];
+        }
+    }
+    return m;
+}
