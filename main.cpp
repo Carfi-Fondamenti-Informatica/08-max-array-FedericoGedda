7eccae1c33c0d7c0c539d8a9b54bd338bb2f7149
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-12-12 09:04:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+   int n;
+   cin>>n;
+   float a[n],m;
+   for (int c=0;c<n;c++){
+       cin>>a[c];
+   }
+   m=mass(n,a);
+   cout<<m;
+   return 0;
 }
+
